4784d7ad3fb257d2810e4bdb4fe3806cb80397a2
MMZBin <momizibe@outlook.jp>
2025-03-29 15:14:10 +0900

    順番を変更

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -18,14 +18,14 @@ const Page = () => {
             <p className="text-3xl m-3 drop-shadow-md">もいずる(moizlu)</p>
 
             <ContactCard iconPath="/logo/moizlu-white.svg" serviceName="Portfolio" userName="制作中" url="https://moizlu.com" />
-            <ContactCard iconPath="/logo/youtube.svg" serviceName="YouTube" userName="@moizlu" url="https://www.youtube.com/@moizlu" />
-            <ContactCard iconPath="/logo/x.svg" serviceName="X(Twitter)" userName="@moizlu" url="https://x.com/moizlu"/>
-            <ContactCard iconPath="/logo/instagram.svg" serviceName="Instagram" userName="@moizlu" url="https://www.instagram.com/moizlu"/>
-            <ContactCard iconPath="/logo/github.svg" serviceName="GitHub" userName="@moizlu" url="https://github.com/moizlu"/>
-            <ContactCard iconPath="/logo/discord.svg" serviceName="Discord" userName="@moizlu"/>
-            <ContactCard iconPath="/logo/steam.svg" serviceName="Steam" userName="moizlu" url="https://steamcommunity.com/id/moizlu"/>
-            <ContactCard iconPath="/logo/pixiv.svg" serviceName="pixiv" userName="moizlu" url="https://pixiv.me/moizlu"/>
             <ContactCard iconPath="/logo/mail.svg" serviceName="Mail" userName={generateMailAddress("contact", "moizlu", "com")} isMailAddress={true} />
+            <ContactCard iconPath="/logo/discord.svg" serviceName="Discord" userName="@moizlu"/>
+            <ContactCard iconPath="/logo/github.svg" serviceName="GitHub" userName="@moizlu" url="https://github.com/moizlu" />
+            <ContactCard iconPath="/logo/pixiv.svg" serviceName="pixiv" userName="moizlu" url="https://pixiv.me/moizlu" />
+            <ContactCard iconPath="/logo/x.svg" serviceName="X(Twitter)" userName="@moizlu" url="https://x.com/moizlu" />
+            <ContactCard iconPath="/logo/youtube.svg" serviceName="YouTube" userName="@moizlu" url="https://www.youtube.com/@moizlu" />
+            <ContactCard iconPath="/logo/instagram.svg" serviceName="Instagram" userName="@moizlu" url="https://www.instagram.com/moizlu" />
+            <ContactCard iconPath="/logo/steam.svg" serviceName="Steam" userName="moizlu" url="https://steamcommunity.com/id/moizlu"/>
         </div>
     );
 }
