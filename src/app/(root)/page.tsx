5e9e337000aaa01f8aa0185f9dc8334fc3567c23
MMZBin <momizibe@outlook.jp>
2025-03-29 15:36:26 +0900

    ユーザー名の調整

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -19,12 +19,12 @@ const Page = () => {
 
             <ContactCard iconPath="/logo/moizlu-white.svg" serviceName="Portfolio" userName="制作中" url="https://moizlu.com" />
             <ContactCard iconPath="/logo/mail.svg" serviceName="Mail" userName={generateMailAddress("contact", "moizlu", "com")} isMailAddress={true} />
-            <ContactCard iconPath="/logo/discord.svg" serviceName="Discord" userName="@moizlu"/>
-            <ContactCard iconPath="/logo/github.svg" serviceName="GitHub" userName="@moizlu" url="https://github.com/moizlu" />
+            <ContactCard iconPath="/logo/discord.svg" serviceName="Discord" userName="moizlu"/>
+            <ContactCard iconPath="/logo/github.svg" serviceName="GitHub" userName="moizlu" url="https://github.com/moizlu" />
             <ContactCard iconPath="/logo/pixiv.svg" serviceName="pixiv" userName="moizlu" url="https://pixiv.me/moizlu" />
             <ContactCard iconPath="/logo/x.svg" serviceName="X(Twitter)" userName="@moizlu" url="https://x.com/moizlu" />
             <ContactCard iconPath="/logo/youtube.svg" serviceName="YouTube" userName="@moizlu" url="https://www.youtube.com/@moizlu" />
-            <ContactCard iconPath="/logo/instagram.svg" serviceName="Instagram" userName="@moizlu" url="https://www.instagram.com/moizlu" />
+            <ContactCard iconPath="/logo/instagram.svg" serviceName="Instagram" userName="moizlu" url="https://www.instagram.com/moizlu" />
             <ContactCard iconPath="/logo/steam.svg" serviceName="Steam" userName="moizlu" url="https://steamcommunity.com/id/moizlu"/>
         </div>
     );
