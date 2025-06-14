b2681734d0b7bd0b9ea3f035ee8a5f2431439f41
もいずる <momizibe@outlook.jp>
2025-06-15 01:00:58 +0900

    コピーボタンの色を修正

--- src/components/Svelte/ui/CopyButton/CopyButton.svelte
+++ src/components/Svelte/ui/CopyButton/CopyButton.svelte
@@ -12,7 +12,7 @@
 <button class={clsx(className, `flex justify-center items-center
                                 transition-all duration-300
                                 w-15 h-15 m-2 p-3
-                                bg-zinc-800 dark:bg-zinc-300
+                                bg-zinc-800 dark:bg-zinc-100
                                 shadow-zinc-500 shadow-md
                                 active:bg-zinc-300 dark:active:bg-zinc-600`
 )}
