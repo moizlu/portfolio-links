18bfa1c1e22da1bb4de27d9ad1095e97b0908961
もいずる <momizibe@outlook.jp>
2025-06-15 14:54:55 +0900

    レスポンシブデザインを強化

--- src/components/Svelte/ui/ShareButton/ShareButton.svelte
+++ src/components/Svelte/ui/ShareButton/ShareButton.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <!-- 共有ボタン -->
-<button class="fixed top-0 right-0 m-5 z-105" {onclick}>
+<button class="fixed top-0 right-2 sm:right-[calc(50%-var(--breakpoint-sm)/2)] m-5 z-105" {onclick}>
     <img src="/icons/light/share.svg" alt="share" width={70} class="drop-shadow-zinc-100 drop-shadow-sm block dark:hidden">
     <img src="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-zinc-800 drop-shadow-sm hidden dark:block">
 </button>
@@ -21,7 +21,7 @@
                 p-5 pt-10
                 bg-zinc-100 dark:bg-zinc-800
                 shwdow-white dark:shadow-zinc-950 shadow-lg
-                scale-75 sm:scale-100
+                scale-50 xs:scale-75 sm:scale-100
     ">
         <img src="/icons/light/home-qr.svg" alt="QR code" width={300} class="block dark:hidden">
         <img src="/icons/dark/home-qr.svg" alt="QR code" width={300} class="hidden dark:block">
