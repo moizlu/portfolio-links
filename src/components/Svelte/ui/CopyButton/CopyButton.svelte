25e3b112c89e3b39e046e31420f792c133add44f
もいずる <momizibe@outlook.jp>
2025-06-18 22:17:48 +0900

    改善

--- src/components/Svelte/ui/CopyButton/CopyButton.svelte
+++ src/components/Svelte/ui/CopyButton/CopyButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import clsx from 'clsx';
+    import Icon from '../Icon/Icon.svelte';
 
     interface Props {
         url: string;
@@ -12,12 +13,12 @@
 <button class={clsx(className, `flex justify-center items-center
                                 transition-all duration-300
                                 w-15 h-15 m-2 p-3
-                                bg-zinc-800 dark:bg-zinc-100
-                                shadow-zinc-500 shadow-md
-                                active:bg-zinc-300 dark:active:bg-zinc-600`
+                                bg-accent
+                                hover:bg-primary active:bg-primary
+                                border-accent border-1
+                                `
 )}
         onclick={() => { navigator.clipboard.writeText(url); }}>
 
-    <img src="/icons/light/copy.svg" alt="copy" width={size} class="block dark:hidden" />
-    <img src="/icons/dark/copy.svg" alt="copy" width={size} class="hidden dark:block" />
+    <Icon lightSrc="/icons/light/copy.svg" darkSrc="/icons/dark/copy.svg" alt="copy" width={size} class="drop-shadow-accent drop-shadow-sm" />
 </button>
--- src/components/Svelte/ui/ShareButton/ShareButton.svelte
+++ src/components/Svelte/ui/ShareButton/ShareButton.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Modal from "../Modal/Modal.svelte";
     import CopyButton from "../CopyButton/CopyButton.svelte";
+    import Icon from "../Icon/Icon.svelte";
 
     let isOpen: boolean = $state(false);
 
@@ -11,15 +12,14 @@
 
 <!-- 共有ボタン -->
 <button class="fixed top-0 right-2 sm:right-[calc(50%-var(--breakpoint-sm)/2)] m-5 z-105" {onclick}>
-    <img src="/icons/light/share.svg" alt="share" width={70} class="drop-shadow-zinc-100 drop-shadow-sm block dark:hidden">
-    <img src="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-zinc-800 drop-shadow-sm hidden dark:block">
+    <Icon lightSrc="/icons/light/share.svg" darkSrc="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-accent drop-shadow-sm" />
 </button>
 
 <!-- モーダル -->
 <Modal bind:isOpen={isOpen}>
     <div class="flex flex-col justify-center items-center
                 p-5 pt-10
-                bg-zinc-100 dark:bg-zinc-800
+                bg-primary
                 shwdow-white dark:shadow-zinc-950 shadow-lg
                 scale-50 xs:scale-75 sm:scale-100
     ">
