3396f5bd08a99600e6a5deebab925a0207710ef8
もいずる <momizibe@outlook.jp>
2025-06-24 21:46:51 +0900

    モーダルの改善

--- src/components/Svelte/ui/Modal/Modal.svelte
+++ src/components/Svelte/ui/Modal/Modal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import type { Snippet } from 'svelte';
+    import { fade } from 'svelte/transition';
+    import type { Snippet } from 'svelte';
 
     interface Props {
         isOpen: boolean;
@@ -39,16 +40,16 @@
         {@render children()}
     </button>
 {/if} -->
-<div class={[`fixed top-0 left-0 w-full h-full z-10
-              transition-opacity duration-200
-              flex items-center justify-center
-              bg-zinc-50 dark:bg-zinc-900 blur-2xl
-`, (isOpen) ? "opacity-75" : "opacity-0 pointer-events-none"]}>
-</div>
 
 {#if isOpen}
-    <button class="fixed top-0 left-0 w-full h-full z-10
-                   transition-opacity duration-200
+    <div transition:fade={{duration: 150}} class="fixed top-0 left-0 w-full h-full z-10
+                  transition-opacity duration-200
+                  flex items-center justify-center
+                  bg-zinc-50 dark:bg-zinc-900 opacity-75">
+    </div>
+
+    <button transition:fade={{duration: 150}} class="fixed top-0 left-0 w-full h-full z-10
+                   transition-opacity duration-200 backdrop-blur-sm
                    flex items-center justify-center"
         bind:this={backgroundElement}
         onclick={onclick}
--- src/components/Svelte/ui/ShareButton/ShareButton.svelte
+++ src/components/Svelte/ui/ShareButton/ShareButton.svelte
@@ -12,13 +12,13 @@
 
 <!-- 共有ボタン -->
 <button class="fixed top-0 right-2 sm:right-[calc(50%-var(--breakpoint-sm)/2)] m-5 z-105" {onclick}>
-    <Icon lightSrc="/icons/light/share.svg" darkSrc="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-accent drop-shadow-sm" />
+    <Icon lightSrc="/icons/light/share.svg" darkSrc="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-primary drop-shadow-sm" />
 </button>
 
 <!-- モーダル -->
 <Modal bind:isOpen={isOpen}>
     <div class="flex flex-col justify-center items-center
-                p-5 pt-10
+                p-5 pt-10 m-3
                 bg-primary
                 shwdow-white dark:shadow-zinc-950 shadow-lg
                 scale-50 xs:scale-75 sm:scale-100
@@ -27,8 +27,8 @@
         <img src="/icons/dark/home-qr.svg" alt="QR code" width={300} class="hidden dark:block">
 
         <div class="flex justify-center items-center gap-4">
-            <p class="text-4xl">https://moiz.lu/</p>
-            <CopyButton className="m-10" url="https://moiz.lu/" />
+            <p class="text-4xl m-2">https://moiz.lu/</p>
+            <CopyButton url="https://moiz.lu/" />
         </div>
     </div>
 </Modal>
