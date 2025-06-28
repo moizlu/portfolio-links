6d63c338ecd7e7c75bfdb113cd6e4a5085315e82
もいずる <momizibe@outlook.jp>
2025-06-29 04:39:37 +0900

    共有ボタンの改善

--- src/components/Svelte/ui/ShareButton/ShareButton.svelte
+++ src/components/Svelte/ui/ShareButton/ShareButton.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { fade } from "svelte/transition";
+
     import Modal from "../Modal/Modal.svelte";
     import CopyButton from "../CopyButton/CopyButton.svelte";
     import Icon from "../Icon/Icon.svelte";
@@ -12,7 +14,11 @@
 
 <!-- 共有ボタン -->
 <button class="fixed top-0 right-2 sm:right-[calc(50%-var(--breakpoint-sm)/2)] m-5 z-105" {onclick}>
-    <Icon lightSrc="/icons/light/share.svg" darkSrc="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-primary drop-shadow-sm" />
+    {#if isOpen}
+        <Icon lightSrc="/icons/light/close.svg" darkSrc="/icons/dark/close.svg" alt="share" width={70} class="drop-shadow-primary drop-shadow-sm" />
+    {:else}
+        <Icon lightSrc="/icons/light/share.svg" darkSrc="/icons/dark/share.svg" alt="share" width={70} class="drop-shadow-primary drop-shadow-sm" />
+    {/if}
 </button>
 
 <!-- モーダル -->
@@ -23,8 +29,8 @@
                 shwdow-white dark:shadow-zinc-950 shadow-lg
                 scale-50 xs:scale-75 sm:scale-100
     ">
-        <img src="/icons/light/home-qr.svg" alt="QR code" width={300} class="block dark:hidden">
-        <img src="/icons/dark/home-qr.svg" alt="QR code" width={300} class="hidden dark:block">
+
+        <Icon lightSrc="/icons/light/home-qr.svg" darkSrc="/icons/dark/home-qr.svg" alt="QR code" width={300} height={300} />
 
         <div class="flex justify-center items-center gap-4">
             <p class="text-4xl m-2">https://moiz.lu/</p>
