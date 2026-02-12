bc6623306a6635dfeb6b744db6f3e2ae33721174
もいずる <momizibe@outlook.jp>
2026-02-12 11:35:35 +0900

    手動で戻るように変更

---
+++ src/lib/assets/icons/arrow.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { SvgComponentProps } from "$lib/types";
+    const { size,  class: className }: SvgComponentProps = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" height={size} viewBox="0 -960 960 960" width={size} class={className}>
+    <path d="m 102.805,-315.115 372.31,-372.31 372.31,372.31 -42.54,42.54 -329.77,-329.77 -329.77,329.77 z" />
+</svg>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
+    import ArrowIcon from "$lib/assets/icons/arrow.svelte";
+
     import { browser } from "$app/environment";
     import { goto } from "$app/navigation";
     import { page } from "$app/state";
 
-    if (browser) {
-        if (page.status === 404) {
-            goto('/');
-        }
-    }
+    import SvgIcon from "$lib/components/ui/SvgIcon";
+
+    // if (browser) {
+    //     if (page.status === 404) {
+    //         goto('/');
+    //     }
+    // }
 </script>
 
-<div class="w-full h-dvh flex-center">
-    <h1>エラー:{page.status}</h1>
+<div class="w-full h-dvh flex-col-center gap-2">
+    <h1>{page.status}</h1>
+    {page.url.pathname}
+    {#if page.status === 404}
+        <p class="text-center">URLが見つかりませんでした。<br>入力ミスが無いか確認してください。</p>
+    {/if}
+
+    <a href="/" title="back to root" class="w-60 p-2 flex justify-start items-center rounded-full button-general">
+        <SvgIcon Svg={ArrowIcon} size={30} class="rotate-270 text-label" />
+        <p class="flex-1 text-center">ホームに戻る</p>
+    </a>
 </div>
