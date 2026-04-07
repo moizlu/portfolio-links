7375627b6ae281b299c5aa8c41704b8f1456d696
もいずる <momizibe@outlook.jp>
2026-04-07 22:14:16 +0900

    Update +page.svelte

--- src/routes/discord/+page.svelte
+++ src/routes/discord/+page.svelte
@@ -14,6 +14,7 @@
     let isCopied = $state(false);
 
     const onUserNameCopyClick = () => {
+        navigator.clipboard.writeText("moizlu");
         isCopied = true;
         setTimeout(() => {
             isCopied = false;
