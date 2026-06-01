7c929ca2f675cb69bf7f40bd350616ff364ac2dd
もいずる <momizibe@outlook.jp>
2026-06-01 21:52:03 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import type { Pathname } from '$app/types';
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	// import type { Pathname } from '$app/types';
+	// import { resolve } from '$app/paths';
+	// import { page } from '$app/state';
+	// import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import { getLocale } from '$lib/paraglide/runtime';
 
 	import './layout.css';
@@ -54,10 +54,10 @@
 
 {@render children()}
 
-<div style="display:none">
+<!-- <div style="display:none">
 	{#each locales as locale (locale)}
 		<a
 			href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}
 		>{locale}</a>
 	{/each}
-</div>
+</div> -->
